d14dfff4a058010f6d825115e0ffaccbbca06855
Alan Jones Rios <alan.jones.rios@gmail.com>
2024-04-03 11:45:07 -0300

    init

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import '@/styles/globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ClerkProvider
+    signInUrl="https://mentorfy.com.br/"
+      isSatellite={(url) =>
+        !url.hostname.includes("mentorfy") &&
+        !url.hostname.includes("localhost")
+      }
+      domain={(url) => {
+        if (url.hostname.includes("mentorfy.")) {
+          return "mentorfy.com.br";
+        }
+        return `${url.hostname.replace("app.", "")}`;
+      }}
+      localization={location}
+    {...pageProps}>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  )
+}
+
+export default MyApp;
